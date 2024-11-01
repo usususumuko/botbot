543b13995f137e9159459744eb83605b80767acb
usususumuko <kkonf79@gmail.com>
2024-11-01 07:54:54 +0700

    Create set.py

---
+++ set.py
@@ -0,0 +1,11 @@
+import os
+    os.system("pip3 install cloudscraper")
+    os.system("pip3 install socks")
+    os.system("pip3 install pysocks")
+    os.system("pip3 install colorama")
+    os.system("pip3 install undetected_chromedriver")
+    os.system("pip3 install httpx")
+    os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
+    os.system("apt-get install ./google-chrome-stable_current_amd64.deb")
+
+print("Done.")
