3ffb04a516ea633aa8566305551348941d92dd2f
usususumuko <kkonf79@gmail.com>
2024-11-01 07:56:49 +0700

    Update set.py

--- set.py
+++ set.py
@@ -7,5 +7,7 @@ import os
     os.system("pip3 install httpx")
     os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
     os.system("apt-get install ./google-chrome-stable_current_amd64.deb")
+    os.system("wget https://raw.githubusercontent.com/usususumuko/botbot/refs/heads/main/bot.py")
+
 
 print("Done.")
